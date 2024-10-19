2cf4b889aa20918dd46ab649571416acb5c78d98
Eduardo Higuti <eduardohiguti@outlook.com>
2024-10-19 18:13:47 -0300

    change hardcoded objects to texture

--- src/main.zig
+++ src/main.zig
@@ -7,7 +7,6 @@ const SCREEN_HEIGHT = 600;
 
 pub fn main() anyerror!void {
     rl.initWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Big Bosta");
-    const font = rl.loadFont("resources/comic.ttf");
     defer rl.closeWindow();
 
     // AUDIO
@@ -20,6 +19,7 @@ pub fn main() anyerror!void {
     var timePlayed: f32 = 0.0;
     var pause = false;
 
+    // MAIN GAME LOOP
     while (!rl.windowShouldClose()) {
         rl.clearBackground(rl.Color.black);
         rl.hideCursor();
@@ -43,7 +43,7 @@ pub fn main() anyerror!void {
 
         rl.drawFPS(700, 580);
 
-        hitcircle(200, 300, 40);
+        hitcircle(200, 300);
 
         //TODO implementar score de algum jeito
         //const score = 50;
@@ -51,45 +51,50 @@ pub fn main() anyerror!void {
         rl.drawRectangle(10, 50, @intFromFloat(timePlayed * 200), 12, rl.Color.white);
 
         //Right key
-        const rightKey = rl.Vector2.init(774, 265);
-        var rightKeyCount: i32 = 0;
-        rl.drawRectangle(765, 265, 30, 30, rl.Color.white);
-        if (rl.isKeyDown(rl.KeyboardKey.key_x)) {
-            rl.drawRectangle(763, 263, 34, 34, rl.Color.magenta);
-            rightKeyCount += 1;
-        }
-        rl.drawTextEx(font, "0", rightKey, 30, 20, rl.Color.black);
-
+        keyCounter(774, 265, true);
         //Left key
-        const leftKey = rl.Vector2.init(774, 305);
-        var leftKeyCount: i32 = 0;
-        rl.drawRectangle(765, 305, 30, 30, rl.Color.white);
-        if (rl.isKeyDown(rl.KeyboardKey.key_z)) {
-            rl.drawRectangle(763, 303, 34, 34, rl.Color.magenta);
-            leftKeyCount += 1;
-        }
-        rl.drawTextEx(font, "0", leftKey, 30, 20, rl.Color.black);
+        keyCounter(774, 305, false);
 
         //Cursor
+        //true or false to toggle hit explosion
         cursorInit(true);
     }
 
     rl.unloadMusicStream(music);
     rl.closeAudioDevice();
-    rl.unloadFont(font);
 }
 
-fn cursorInit(ActivateHitExplosion: bool) void {
+fn keyCounter(posX: f32, posY: f32, righKey: bool) void {
+    const baseSquarePosition = rl.Vector2.init(posX, posY);
+    const baseSquareSize = rl.Vector2.init(30, 30);
+    const keyPosition = rl.Vector2.init(posX + 4, posY + 3);
+    var keyCount: i32 = 0;
+    rl.drawRectangleV(baseSquarePosition, baseSquareSize, rl.Color.white);
+    if (righKey) {
+        if (rl.isKeyDown(rl.KeyboardKey.key_x)) {
+            rl.drawRectangleV(baseSquarePosition, baseSquareSize, rl.Color.magenta);
+            keyCount += 1;
+        }
+    } else {
+        if (rl.isKeyDown(rl.KeyboardKey.key_z)) {
+            rl.drawRectangleV(baseSquarePosition, baseSquareSize, rl.Color.magenta);
+            keyCount += 1;
+        }
+    }
+    rl.drawTextEx(rl.getFontDefault(), "0", keyPosition, 20, 20, rl.Color.black);
+}
+
+fn cursorInit(activateHitExplosion: bool) void {
     var cursorPosition = rl.Vector2.init(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
     cursorPosition = rl.getMousePosition();
     rl.drawCircleV(cursorPosition, 20, rl.Color.yellow);
 
-    if (ActivateHitExplosion) {
-        hit_explosion(cursorPosition);
+    if (activateHitExplosion) {
+        hitExplosion(cursorPosition);
     }
 }
 
-fn hit_explosion(cursorPosition: rl.Vector2) void {
+fn hitExplosion(cursorPosition: rl.Vector2) void {
     //Right key
     if (rl.isKeyDown(rl.KeyboardKey.key_x)) {
         rl.drawCircleV(cursorPosition, 80, rl.fade(rl.Color.pink, 0.1));
@@ -100,31 +105,32 @@ fn hit_explosion(cursorPosition: rl.Vector2) void {
     }
 }
 
-fn hitcircle(posX: f32, posY: f32, radius: f32) void {
-    const HitCirclePosition = rl.Vector2.init(posX, posY);
-    rl.drawCircleV(HitCirclePosition, radius, rl.Color.white);
+fn hitcircle(posX: f32, posY: f32) void {
+    const hitcirclePosition = rl.Vector2.init(posX, posY);
+
+    const hitcircleImage = rl.loadImage("resources/hitcircle.png");
+    const hitcircleTexture = rl.loadTextureFromImage(hitcircleImage);
+    rl.drawTextureV(hitcircleTexture, hitcirclePosition, rl.Color.green);
 
     hitcircleNumber(posX, posY);
 
-    approachCircle(posX, posY, radius);
+    approachCircle(posX, posY);
+    rl.unloadImage(hitcircleImage);
 }
 
 fn hitcircleNumber(posX: f32, posY: f32) void {
     const default_1 = rl.loadImage("resources/default-1.png");
     const num1 = rl.loadTextureFromImage(default_1);
-    const numberPosition = rl.Vector2.init(posX - 20, posY - 25);
+    const numberPosition = rl.Vector2.init(posX + 37, posY + 35);
 
-    rl.drawTextureV(num1, numberPosition, rl.Color.black);
+    rl.drawTextureV(num1, numberPosition, rl.Color.white);
     rl.unloadImage(default_1);
 }
 
-fn approachCircle(posX: f32, posY: f32, radius: f32) void {
-    const ApproachCirclePosition = rl.Vector2.init(posX, posY);
-    const innerRadius = radius;
-    const outerRadius = radius + 5;
-    const startAngle = 0.0;
-    const endAngle = 360.0;
-    const segments = 0.0;
+fn approachCircle(posX: f32, posY: f32) void {
+    const approachCirclePosition = rl.Vector2.init(posX, posY);
 
-    rl.drawRing(ApproachCirclePosition, innerRadius, outerRadius, startAngle, endAngle, segments, rl.Color.maroon);
+    const approachCircleImage = rl.loadImage("resources/approachcircle.png");
+    const approachCircleTexture = rl.loadTextureFromImage(approachCircleImage);
+    rl.drawTextureV(approachCircleTexture, approachCirclePosition, rl.Color.white);
 }
