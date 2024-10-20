dd7c9d9d41ba6d2e198545264491f624186cb9f1
Eduardo Higuti <eduardohiguti@outlook.com>
2024-10-19 23:37:58 -0300

    function keyCounter updated now it really counts when the keys is pressed lol

--- src/main.zig
+++ src/main.zig
@@ -28,8 +28,13 @@ pub fn main() anyerror!void {
     const approachCircleImage = rl.loadImage("resources/approachcircle.png");
     const approachCircleTexture = rl.loadTextureFromImage(approachCircleImage);
 
+    const score: i32 = 1000000;
+
     // MAIN GAME LOOP
     while (!rl.windowShouldClose()) {
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
         rl.clearBackground(rl.Color.black);
         rl.hideCursor();
 
@@ -47,22 +52,16 @@ pub fn main() anyerror!void {
 
         timePlayed = rl.getMusicTimePlayed(music) / rl.getMusicTimeLength(music);
 
-        rl.beginDrawing();
-        defer rl.endDrawing();
-
         rl.drawFPS(700, 580);
 
         hitcircle(200, 300, hitcircleTexture, num1Texture, approachCircleTexture);
 
         //TODO implementar score de algum jeito
-        //const score = 50;
-        rl.drawText("1,000,000", 10, 10, 40, rl.Color.white);
+
+        rl.drawText(rl.textFormat("Score: %d", .{score}), 10, 10, 40, rl.Color.white);
         rl.drawRectangle(10, 50, @intFromFloat(timePlayed * 200), 12, rl.Color.white);
 
-        //Right key
-        keyCounter(774, 265, true);
-        //Left key
-        keyCounter(774, 305, false);
+        keyCounter();
 
         //Cursor
         //true or false to toggle hit explosion
@@ -76,24 +75,27 @@ pub fn main() anyerror!void {
     rl.closeAudioDevice();
 }
 
-fn keyCounter(posX: f32, posY: f32, righKey: bool) void {
-    const baseSquarePosition = rl.Vector2.init(posX, posY);
-    const baseSquareSize = rl.Vector2.init(30, 30);
-    const keyPosition = rl.Vector2.init(posX + 4, posY + 3);
-    var keyCount: i32 = 0;
-    rl.drawRectangleV(baseSquarePosition, baseSquareSize, rl.Color.white);
-    if (righKey) {
-        if (rl.isKeyDown(rl.KeyboardKey.key_x)) {
-            rl.drawRectangleV(baseSquarePosition, baseSquareSize, rl.Color.magenta);
-            keyCount += 1;
-        }
-    } else {
-        if (rl.isKeyDown(rl.KeyboardKey.key_z)) {
-            rl.drawRectangleV(baseSquarePosition, baseSquareSize, rl.Color.magenta);
-            keyCount += 1;
-        }
-    }
-    rl.drawTextEx(rl.getFontDefault(), "0", keyPosition, 20, 20, rl.Color.black);
+var xKeyCount: i32 = 0;
+var zKeyCount: i32 = 0;
+
+fn keyCounter() void {
+    const rightSquarePosition = rl.Vector2.init(770, 265);
+    const leftSquarePosition = rl.Vector2.init(770, 305);
+    const squareSize = rl.Vector2.init(30, 30);
+    const rightKeyPosition = rl.Vector2.init(778, 268);
+    const leftKeyPosition = rl.Vector2.init(778, 308);
+
+    //Right key
+    rl.drawRectangleV(rightSquarePosition, squareSize, rl.Color.white);
+    if (rl.isKeyDown(rl.KeyboardKey.key_x)) rl.drawRectangleV(rightSquarePosition, squareSize, rl.Color.magenta);
+    if (rl.isKeyPressed(rl.KeyboardKey.key_x)) xKeyCount += 1;
+    rl.drawTextEx(rl.getFontDefault(), rl.textFormat("%d", .{xKeyCount}), rightKeyPosition, 15, 1, rl.Color.black);
+
+    //Left key
+    rl.drawRectangleV(leftSquarePosition, squareSize, rl.Color.white);
+    if (rl.isKeyDown(rl.KeyboardKey.key_z)) rl.drawRectangleV(leftSquarePosition, squareSize, rl.Color.magenta);
+    if (rl.isKeyPressed(rl.KeyboardKey.key_z)) zKeyCount += 1;
+    rl.drawTextEx(rl.getFontDefault(), rl.textFormat("%d", .{zKeyCount}), leftKeyPosition, 15, 1, rl.Color.black);
 }
 
 fn cursorInit(activateHitExplosion: bool) void {
